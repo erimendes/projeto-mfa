af95dde4e6abfad14fe3234eb5563e7f063894ee
erimendes <eri.mendes32@gmail.com>
2025-01-17 17:00:26 -0300

    segundo commit

--- meu_projeto/meu_projeto/settings.py
+++ meu_projeto/meu_projeto/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'meu_projeto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'nome_do_banco',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'mongodb://localhost:27017',  # Endereço do seu servidor MongoDB
+        },
     }
 }
 
